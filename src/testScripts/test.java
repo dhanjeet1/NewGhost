65a730cbee37eddd3dd283a36141a69bf23d782c
dhanjeetsvst <dhanjeetsvst@JTGDT12.IN.nam.ad.pwcinternal.com>
2017-12-13 13:37:36 +0530

    Changes

--- src/testScripts/test.java
+++ src/testScripts/test.java
@@ -2,6 +2,7 @@ package testScripts;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -20,16 +21,15 @@ public class test
 	@Parameters({"CONFIGFILE", "baseurl"})
     public void login(String CONFIGFILE, String baseurl) throws InterruptedException
 	{
-		System.out.println("+++++++++++++++++++++Changed +++++++");
 		//*******Initializing Local Variables to Data members*********//
 	
-		
 		driver = ConfigFile.getWebDriver(CONFIGFILE);
 		
 		driver.get(baseurl);
 		Thread.sleep(2000);
-		System.out.println(driver.findElement(By.xpath("//*[contains(text(),'Log in')]")).getText());
-		Assert.assertEquals(driver.findElement(By.xpath("//*[contains(text(),'Log in')]")).getText(), "Log in");
+		WebElement gmailLink = driver.findElement(By.xpath("//*[contains(text(),'Gmail')]"));
+		System.out.println(gmailLink.getText());
+		Assert.assertEquals(gmailLink.getText(), "Gmail");
 		
 		//************Take Screenshot**********//
 	//	ConfigFile.takeSelfie(driver);
